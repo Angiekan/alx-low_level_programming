5d7889925e181e3b596681d494c9c78c326b4b0f
Angiekan <angelajk496@gmail.com>
2023-06-20 11:13:32 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @n: integer to make absolute
+ * Return: int or int times negative
+ *
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else
+		return (c * -1);
+}
