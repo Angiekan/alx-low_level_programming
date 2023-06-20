a588dd524a6e3d8c02175fe77a6669e0891a6e9a
Angiekan <angelajk496@gmail.com>
2023-06-20 07:27:41 -0700

    I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,16 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-    char c;
-    for (c = 'a'; c <= 'i'; c++)
-    {
-    _putchar(c);
-    }
-    _putchar('\n');
+
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+   	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
